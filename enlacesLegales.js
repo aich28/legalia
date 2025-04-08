465f8796c36dd00a34e79caad01f974b5bc540bc
aich28 <126810216+aich28@users.noreply.github.com>
2025-04-08 13:53:08 +0200

    Update enlacesLegales.js

--- enlacesLegales.js
+++ enlacesLegales.js
@@ -1,10 +1,1 @@
-export const generarEnlacesLegales = (terminoBusqueda) => {
-  const encoded = encodeURIComponent(terminoBusqueda);
 
-  return {
-    boe: `https://www.boe.es/buscar/?q=${encoded}`,
-    cendoj: `https://www.poderjudicial.es/search/indexAN.jsp?tipoBusqueda=CONTENIDO&contenido=${encoded}`,
-    aeat: `https://sede.agenciatributaria.gob.es/Sede/consulta-vinculante.html?q=${encoded}`,
-    tributos: `https://petete.minhafp.gob.es/consultas/?texto=${encoded}`
-  };
-};
